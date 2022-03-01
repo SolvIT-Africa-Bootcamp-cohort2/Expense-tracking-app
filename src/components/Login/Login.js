f1598e018f7449d630d12501a1e04d874bf70d17
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-03-01 14:49:48 +0200

    created login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,44 @@
 import React from "react";
+import "../../styles/login.scss";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login-main-container">
+      <div className="login-background-container">
+        <div></div>
+        <div></div>
+      </div>
+      <div className="form-main-container">
+        <img src={require("../../assets/logo.png")} alt="logo" />
+        <div className="form-container">
+          <h2>Welcome to XPENSE</h2>
+          <p>Login to continue</p>
+          <form method="post">
+            <div className="form-group">
+              <label>Email</label>
+              <input className="form-control" placeholder="Enter your email" />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                className="form-control"
+                placeholder="Enter your password"
+              />
+            </div>
+            <button>Login</button>
+          </form>
+          <div className="forget-password-container">
+            <a>Forget password?</a>
+          </div>
+          <hr />
+          <div className="form-footer2">
+            <span>New?</span>
+            <a>CREATE ACCOUNT</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
