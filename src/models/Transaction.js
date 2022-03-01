2ccf7b13039e5aebc82af2ee2d579901786360e2
Rukundo Kevin <kevinrukundo1@gmail.com>
2022-03-01 10:57:38 +0200

    add Procfile

---
+++ src/models/Transaction.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Joi = require("@hapi/joi")
+
+const schema = mongoose.Schema({
+    description: {type:String, required:[true,"Expense Description is required"]},
+    amount:{type: Number, required: true},
+    category:{type:String, required:true},
+    type:{type:String, required:true},
+    userId:{type: String, required:true},
+    created_at: {type: Date, default: new Date().toISOString().slice(0,10)}
+},{
+    versionKey:false
+})
+
+
+const Transaction = mongoose.model("Transaction",schema);
+
+const validateTransaction = (transaction) =>{
+    const schema = Joi.object({
+        amount: Joi.number().required(),
+        description: Joi.string().required(),
+        category:Joi.string().required()
+    })
+    return schema.validate(transaction);
+}
+
+module.exports = {
+    Transaction,
+    validateTransaction
+}
---
+++ src/routes/expense.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const router = express.Router();
+
+const { verifyToken } = require("../middlewares/verifyToken");
+const {validateInput} = require("../middlewares/validateInput")
+const {validateTransaction} = require("../models/Transaction")
+const {getExpenses , getOneExpense, getExpenseFromDate, addExpense,deleteExpense, updateExpense} = require("../controllers/expense.js");
+const app = require("..");
+
+router.all("/*",verifyToken)
+
+router.route("/")
+      .get(getExpenses)
+      .post(validateInput(validateTransaction), addExpense)
+
+router.get("/:from-:to",getExpenseFromDate);
+
+router.get("/:id",getOneExpense)
+
+router.put("/:id",updateExpense)     
+router.delete("/:id",deleteExpense)
+
+module.exports = router;
