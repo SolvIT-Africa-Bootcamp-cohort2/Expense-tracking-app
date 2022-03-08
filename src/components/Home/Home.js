22cfa111f15943d5d7fbd8c79b1e5e307fefd9f2
MBABAZI Rightman <rightmanmbabazi@gmail.com>
2022-03-08 11:46:54 +0200

    some edits on homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,12 +6,12 @@ function Home() {
   return (
     <div className='home-main-container'>
         <div className="home-background-container">
-        <div div className='header1' >
+        < div className='header1' >
             <div></div>
             <div></div>
       <div  class="row">
   <div class="col-xl">
-    <Navbar className="justify-content-end" bg="warning" >
+    <Navbar class="navbar navbar-expand-sm bg-warning   fixed-top" >
     <Container>
     <Navbar.Brand href="#home"><img
         src={(".../../assets/logo.png")}
@@ -23,8 +23,14 @@ function Home() {
     </Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#about">About us</Nav.Link>
-      <Nav.Link href="#contact">Contact us</Nav.Link>
+      <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#about">About us</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#section2">Contact us</a>
+    </li>
+    </ul>
       <Nav.Link href="/Login">Login</Nav.Link>
       <Nav.Link href="/register">Register</Nav.Link>
     </Nav>
@@ -32,102 +38,81 @@ function Home() {
   </Navbar>
     </div>
      </div>
+    
      <div class="row">
         <div class="col-md-6">
-          <img className="photo1" src={require("../../assets/home.png")} alt="home" /> 
+          <img className="photo1" src={require("../../assets/home1.png")} alt="home" /> 
         </div>
-        <div class="col-md-6">  
-         <div className="container-xl mt-3 bg-grey">
-        <section className='container pt-5' style={{marginBottom:'10rem'}}>
-          <h1> Managing money.<br></br>
-            made simple.</h1>
-            <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a>
-        </section>
-        <br/> 
-        </div>
-        </div>
-        
-        
-     {/* <div class="col-md-6"> 
-     <div className="container-xl mt-3">
-     <div className="container-xl mt-3 bg-grey">
-        <section className='container pt-5' style={{marginBottom:'10rem'}}>
-          <h1> Managing money.<br></br>
-            made simple.</h1>
-            <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a>
-        </section>
-        <br/> 
-        </div>
-     <div className="container-xl mt-3">
-     <img className="photo1" src={require("../../assets/home.png")} alt="home" /> 
-      </div>
-     </div>
-    <div className="container-xl mt-3 bg-grey">
-        <section className='container pt-5' style={{marginBottom:'10rem'}}>
-          <h1> Managing money.<br></br>
-            made simple.</h1>
-            <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a>
-            <img className="photo1" src={require("../../assets/home.png")} alt="home" /> 
-        </section>
-        <br/>
-        </div> */}
-        <div class="row">
         <div class="col-md-6"> 
-      <img className="photo" src={require("../../assets/expense.png")} alt="Expense" /> 
-       </div>
-     <div class="col-md-6"> 
-     <div className="container-xl mt-3">
-        <section className='container pt-5' style={{marginBottom:'10rem'}}>
-          <h1> Keep every expense <br></br>
-          crystal clear.</h1>
-          <p>Get a full view so you know where to save. Track spending, <br/>
+          <h1> Managing money.<br/>
+            made simple.</h1>
+            <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a>
+            </div>
+     </div>
+         <br/>
+        <div class="row">
+          <div class="col-md-6"> 
+            <img className="photo" src={require("../../assets/expense.png")} alt="Expense" /> 
+           </div>
+          <div class="col-md-6"> 
+             <h1> Keep every expense <br></br>
+                 crystal clear.</h1>
+            <p>Get a full view so you know where to save. Track spending, <br/>
              detect fraud, and keep tabs on rising subscription costs.</p>
             <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a>
-        </section>
-        </div>
-        </div>
-      
-        <br/>
-        <div className="container-xl mt-3">
-        <section className='container pt-5' style={{marginBottom:'10rem'}}>
+         </div> 
+         </div>
+         <br/>
+         <br/>
+        {/* here is the problem  */}
+         <div class="row">
+          <div class="col-md-6">
           <h1> Spend smarter and <br></br>
           save more.</h1>
           <p>EXPENSE™ automatically help find savings you missed, <br/>
              so you can make your money go further without breaking a sweat.</p>
-            <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a>
-            <img className="photo" src={require("../../assets/save.png")} alt="save" /> 
-        </section>
-        </div>
-        </div>
+            <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a> 
+            </div>
+          <div class="col-md-6"> 
+          <img className="photo" src={require("../../assets/save.png")} alt="save" /> 
+            </div>
+            </div>
         <div class="row">
-  <div class="col-md-6">
-  <img className="photo" src={require("../../assets/expense.png")} alt="Expense" /> 
-  </div>
-  <div class="col-md-6">
-  <div className="container-xl mt-3">
-        <section className='container pt-5' style={{marginBottom:'10rem' }}>
+          <div class="col-md-6">
+              <img className="photo" src={require("../../assets/expense.png")} alt="Expense" /> 
+          </div>
+         <div class="col-md-6">
           <h1>Stay focused on your <br></br>
           financial goals.</h1>
           <p>Improve your spending habits with custom goals that keep you going. <br/>
              Save for a home, conquer debt, and prepare for the future.</p>
+            <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a> 
+           </div>
+            </div>
+            <br/> 
+              <div class="row">
+            <div class="col-md-6"> 
+                 <h1>No need for <br/>
+                 multiple apps</h1>
+                 <p>Easily connect all your accounts. <br/>
+                  From cash and credit to loans and investments, <br/>
+                  you can see your complete financial picture in.</p>
             <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a>
-            
-        </section>
+              </div>
+                 <div class="col-md-6"  > 
+                  <img className="photo" src={require("../../assets/expense.png")} alt="Expense" /> 
+                </div>
+             </div>
+             <div class="row">
+              <div class="col-md-6">
+                 <div id="about" class="container-fluid ">
+                   <h1>About us</h1>
+                <p>EXPENSE brings together everything from spending, balances, and budgets <br/>
+                 to your   credit score and more. Access your financial life in one powerful app.</p>
+           </div>
         </div>
-    </div>
-    </div>
-        <br/>
-        <div className="container-xl mt-3">
-        <section className='container pt-5' style={{marginBottom:'10rem' }}>
-          <h1>Stay focused on your <br></br>
-          financial goals.</h1>
-          <p>Improve your spending habits with custom goals that keep you going. <br/>
-             Save for a home, conquer debt, and prepare for the future.</p>
-            <a class="btn btn-primary bg-warning" href="/register" role="button">Register for free</a>
-            <img className="photo" src={require("../../assets/expense.png")} alt="Expense" /> 
-        </section>
-        </div>
-      </div>
+               <div class="col-md-6"> </div>
+             </div>
       <div className="container-fluid text-center bg-warning text-md-left">
         <div className="row">
             <div className="col-md-6 mt-md-0 mt-3">
@@ -154,13 +139,10 @@ function Home() {
     </div>
     <div className="footer-copyright text-center bg-warning py-3">© 2022 Copyright:
         <a href="#"> EXPENSE</a>
-    </div>
-    
+    </div>  
   </div>
   </div>
-  // </div>
-  
-  
+  </div>
   )
 }
 
