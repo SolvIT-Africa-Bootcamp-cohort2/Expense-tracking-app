df933e11f935ca704fe8c75b36f7110b40ef4cf0
Rukundo Kevin <kevinrukundo1@gmail.com>
2022-03-08 12:21:03 +0200

    Change the mongo uri

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ import { blue, red,magenta } from "chalk";
 const log = console.log;
 
 export async function connectDB() {
-    //log(magenta("secret is"+process.env.PASS))
-    await connect("mongodb+srv://MugaboKevin:Rukundo2@cluster0.ybelk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    log(magenta("secret is"+process.env.DB_Host))
+    await connect(process.env.DB_Host, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>  log(blue("DB connected successfully")))
     .catch(err=>  log(red(`Unable to connect to the Mongo db  ${err} `)));
 }
