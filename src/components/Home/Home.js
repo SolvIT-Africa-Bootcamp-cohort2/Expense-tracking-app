7be573be86577bc14b37f81661aac24faa6b5e6d
Mbabazi-Rightma22 <rightmanmbabazi22>
2022-03-22 14:00:08 +0200

    add about us

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -130,7 +130,7 @@ function Home() {
                                 </ul>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link scroll h4 "  href="#contact">Contact</a>
+                                <a class="nav-link scroll h4 "  href="#About">About us</a>
                             </li>
                         </ul>
                     </nav>
@@ -245,7 +245,58 @@ function Home() {
   </div>
   </div> */}
   </div>
-      
+
+    {/* //contact area  */}
+     
+    <section id="About" class="contact-area bg-gray ptb_100">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-10 col-lg-8">
+                        
+                        <div class="section-heading text-center">
+                            <h2 class="text-capitalize">About us</h2>
+                            <p class="d-none d-sm-block mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum obcaecati dignissimos quae quo ad iste ipsum officiis deleniti asperiores sit.</p>
+                            <p class="d-block d-sm-none mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum obcaecati.</p>
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                     
+                        <div class="contact-box text-center">
+                            
+                          
+                            {/* <form id="contact-form" method="POST" action="https://theme-land.com/sapp/demo/assets/php/mail.php">
+                                <div class="row">
+                                    <div class="col-12 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="name" placeholder="Name" required="required">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" name="email" placeholder="Email" required="required">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="subject" placeholder="Subject" required="required">
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <div class="form-group">
+                                            <textarea class="form-control" name="message" placeholder="Message" required="required"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <button class="btn btn-bordered mt-3 mt-sm-4" type="submit">Send Message</button>
+                                    </div>
+                                </div>
+                            </form> */}
+                            <p class="form-message"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
        <footer className='footer' class="footer-area">
            
            <div class="footer-top ptb_100">
