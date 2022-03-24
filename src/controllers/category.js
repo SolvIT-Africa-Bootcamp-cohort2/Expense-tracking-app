dc2072eaad8e4637fa90d1b287b6d8aa9192c3a0
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-03-24 14:44:18 +0200

    fixed delete and update category endpoint issues

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,82 +1,85 @@
-const {Category} = require("../models/Category").default;
-import { red,green } from "chalk";
+const { Category } = require("../models/Category").default;
+import { red, green } from "chalk";
 
-const log = console.log
+const log = console.log;
 
-const getCategories = async (req,res, next) =>{
-    try {
-        const categories = await Category.find({userId: req.user["id"]});
-        res.status(200).send({categories: categories})
-    } catch (error) {
-        log(red(error));
-        res.status(404).send({Message:"No categories were found"})
-    }
-}
-
-const getOneCategory = async (req,res, next) =>{
-    try {
-        const id = req.params.id;
-        const category = await Category.findOne({_id:id});
-        res.status(200).send({category: category})
-    } catch (error) {
-        res.status(404).send({Message:"Category Not Found"})
-    }
-}
-
-const addCategory = async (req,res, next) =>{
+const getCategories = async (req, res, next) => {
   try {
-    const {categoryName } = req.body;
-    
-   const newCategory = new Category({
-       categoryName, userId: req.user["id"]
-   })
-   newCategory.save();
-   res.status(201).send({Message:"Category added successfully"})   
+    const categories = await Category.find({ userId: req.user["id"] });
+    res.status(200).send({ categories: categories });
   } catch (error) {
-   //   log(red(error))
-      res.status(500).send({Message:"Error adding category"})
+    log(red(error));
+    res.status(404).send({ Message: "No categories were found" });
   }
-}
+};
 
-const updateCategory = async(req,res,next) =>{
-	try {
-        let category = await Category.findOne({_id: req.params.id})
-        if (category) {
-            if (req.body.categoryName) {
-                category.categoryName = req.body.categoryName
-            }
-          
-            await category.save()
-            res.status(200).send({Message:"Category Details updated successfully"})
-        }else{
-            res.status(404).send({Message:"Category Not Found"})  
-        }
-	} catch(err) {
-		res.status(404).send({Message: "We couldn't find this category " })
-       // log(red(err));
-	}   
-}
+const getOneCategory = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const category = await Category.findOne({ _id: id });
+    res.status(200).send({ category: category });
+  } catch (error) {
+    res.status(404).send({ Message: "Category Not Found" });
+  }
+};
 
-const deleteCategory = async(req,res, next) =>{
-        try {
-             let id = req.params.id;
-             if(id){
-                await Category.deleteOne({ _id: req.params.id })
-                res.status(202).send({Message:"Category Deleted Successfully"})
-             }
-             else{
-                 res.status(400).send({Message:"Please provide id of category to delete"})
-             }
-        } catch {
-            res.status(404).send({ Message: "This Category doesn't exist!" })
-        }    
-}
+const addCategory = async (req, res, next) => {
+  try {
+    const { categoryName } = req.body;
+
+    const newCategory = new Category({
+      categoryName,
+      userId: req.user["id"],
+    });
+    newCategory.save();
+    res.status(201).send({ Message: "Category added successfully" });
+  } catch (error) {
+    //   log(red(error))
+    res.status(500).send({ Message: "Error adding category" });
+  }
+};
+
+const updateCategory = async (req, res, next) => {
+  try {
+    let category = await Category.findOne({ _id: req.body.id });
+    if (category) {
+      if (req.body.categoryName) {
+        category.categoryName = req.body.categoryName;
+      }
+
+      await category.save();
+      res
+        .status(200)
+        .send({ Message: "Category Details updated successfully" });
+    } else {
+      res.status(404).send({ Message: "Category Not Found" });
+    }
+  } catch (err) {
+    res.status(404).send({ Message: "We couldn't find this category " });
+    // log(red(err));
+  }
+};
+
+const deleteCategory = async (req, res, next) => {
+  try {
+    let id = req.body.id;
+    if (id) {
+      await Category.deleteOne({ _id: req.body.id });
+      res.status(202).send({ Message: "Category Deleted Successfully" });
+    } else {
+      res
+        .status(400)
+        .send({ Message: "Please provide id of category to delete" });
+    }
+  } catch {
+    res.status(404).send({ Message: "This Category doesn't exist!" });
+  }
+};
 
 module.exports = {
-    getCategories,
-    getOneCategory,
-    addCategory,
-    updateCategory,
-    deleteCategory
-}
-
+  getCategories,
+  getOneCategory,
+  addCategory,
+  updateCategory,
+  deleteCategory,
+};
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,20 +1,30 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
-const {validateInput} = require("../middlewares/validateInput")
-const {verifyToken} = require("../middlewares/verifyToken")
-const {getCategories,getOneCategory,addCategory,updateCategory,deleteCategory} = require("../controllers/category")
-const {validateCategory} = require("../models/Category").default
+const { validateInput } = require("../middlewares/validateInput");
+const { verifyToken } = require("../middlewares/verifyToken");
+const {
+  getCategories,
+  getOneCategory,
+  addCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/category");
+const { validateCategory } = require("../models/Category").default;
 
 // router.all("/*",verifyToken)
 
-router.route("/")
-      .get(verifyToken,getCategories)
-      .post(verifyToken,validateInput(validateCategory), addCategory)
+router
+  .route("/")
+  .get(verifyToken, getCategories)
+  .post(verifyToken, validateInput(validateCategory), addCategory);
 
-router.get("/:id",verifyToken,getOneCategory)
+router.get("/:id", verifyToken, getOneCategory);
 
-router.delete("/:id",verifyToken,deleteCategory)
-router.put("/:id",verifyToken,updateCategory)
+// router.delete("/:id",verifyToken,deleteCategory)
+router.post("/remove", verifyToken, deleteCategory);
+
+// router.put("/:id", verifyToken, updateCategory);
+router.post("/update", verifyToken, updateCategory);
 
 module.exports = router;
