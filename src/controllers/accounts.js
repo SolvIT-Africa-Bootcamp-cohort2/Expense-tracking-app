7828931decb4d00204d4fb645d7e5df7c1e0e8f8
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-03-25 08:05:05 +0200

    debugged money account routes

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -1,98 +1,97 @@
-const {Account} = require ('../models/Accounts').default
+const { Account } = require("../models/Accounts").default;
 
 // TO CREATE ACCOUNT
 
+const createAccount = async (req, res) => {
+  try {
+    const { accountName } = req.body;
+    const accountExists = await Account.findOne({
+      userId: req.user["id"],
+      accountName: accountName,
+    });
 
-const  createAccount = async (req, res)=> {
-    try {
-        const  {accountName} = req.body
-        const accountExists = await Account.findOne({userId : req.user ["id"], accountName:accountName});
-             
-            if(accountExists) return res.status(202).send({Message:"Account already exists"})
-            const accounts = new Account ({
-                accountName,userId : req.user["id"]
-            })
-                await accounts.save();
-                res.send ({Message:"Account is successfully created"});
-                // console.log ("Account is successfully created");
-    }
-    catch (error) {
-        // console.log(error)
-        res.status(401).send ({error:error});
-    }
+    if (accountExists)
+      return res.status(202).send({ Message: "Account already exists" });
+    const accounts = new Account({
+      accountName,
+      userId: req.user["id"],
+    });
+    await accounts.save();
+    res.send({ Message: "Account is successfully created" });
+    // console.log ("Account is successfully created");
+  } catch (error) {
+    // console.log(error)
+    res.status(401).send({ error: error });
+  }
 };
 
 // TO GET ALL ACCOUNTS
 
-const getAccounts =async (req,res)=>{
-    try {
-        const accounts = await Account.find({userId : req.user ["id"] });
-        accounts.length > 0?res.status(200).send({accounts:accounts}):res.status(200).send({Message:"No accounts created yet"})
-    } catch (error) {
-        // console.log(error);
-        res.status(404).send ({Message:"No account created yet"})
+const getAccounts = async (req, res) => {
+  try {
+    const accounts = await Account.find({ userId: req.user["id"] });
+    accounts.length > 0
+      ? res.status(200).send({ accounts: accounts })
+      : res.status(200).send({ Message: "No accounts created yet" });
+  } catch (error) {
+    // console.log(error);
+    res.status(404).send({ Message: "No account created yet" });
+  }
+};
+
+// TO GET SINGLE ACCOUNT BY ID
+
+const getOneAccount = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const oneAccount = await Account.findOne({ _id: id });
+    if (oneAccount) {
+      res.status(200).send({ account: oneAccount });
     }
-}
+  } catch (error) {
+    res.send(`Account with id:${req.params.id} is not found`);
+  }
+};
 
-// TO GET SINGLE ACCOUNT BY ID  
+// TO DELETE AN ACCOUNT BY ID
 
+const deleteAccount = async (req, res, next) => {
+  try {
+    const id = req.body.id;
 
+    await Account.deleteOne({ _id: id });
 
-const getOneAccount =async (req,res)=>{
-    try {
-        const id= req.params.id;
-            const oneAccount= await Account.findOne({_id:id}) ;
-            if(oneAccount){
-                res.status(200).send({account:oneAccount});
-            } 
-    } catch (error) {
-        res.send(`Account with id:${req.params.id} is not found`) ;
-    }
-    
-} 
-
-// TO DELETE AN ACCOUNT BY ID 
-
-const deleteAccount= async(req,res,next)=>{
-    
-    try {
-        const id =req.params.id ;
-        
-        await Account.deleteOne( {_id:id});
-        
-         res.status(200).send({Message:"Account is successfully deleted"});
-        //  console.log("Account is successfully deleted")
-    } catch (error) {
-        res.status(404).send({Message:"Account can't be found"}) ;
-    }
-
-}
+    res.status(200).send({ Message: "Account is successfully deleted" });
+    //  console.log("Account is successfully deleted")
+  } catch (error) {
+    res.status(404).send({ Message: "Account can't be found" });
+  }
+};
 
 //TO UPDATE ACCOUNT BY ID
 
-const updateAccount =async (req,res)=>{
-     try {
-        const id = req.params.id;
-        let {accountName} = req.body ;
-        let account =await Account.findOne({_id:id});
-        if(account){
-            if (accountName) account.accountName = accountName ;
-            await account.save();
-            res.status(200).send({Message:"Account is updated"});
-            // console.log("Account updated");
-        } else {
-            res.send ({Message:"Account not found"})
-        }
-     } catch (error) {
-        res.status(404).send({error:"USER CAN'T BE FOUND"});
-     }
-    
- 
-}
+const updateAccount = async (req, res) => {
+  try {
+    const id = req.body.id;
+    let { accountName } = req.body;
+    let account = await Account.findOne({ _id: id });
+    if (account) {
+      if (accountName) account.accountName = accountName;
+      await account.save();
+      res.status(200).send({ Message: "Account is updated" });
+      // console.log("Account updated");
+    } else {
+      res.send({ Message: "Account not found" });
+    }
+  } catch (error) {
+    res.status(404).send({ error: "USER CAN'T BE FOUND" });
+  }
+};
 
-
-
-
-
-
-module.exports = {createAccount,getOneAccount,getAccounts,deleteAccount,updateAccount}; 
+module.exports = {
+  createAccount,
+  getOneAccount,
+  getAccounts,
+  deleteAccount,
+  updateAccount,
+};
--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,22 +1,22 @@
-const express = require("express")
-const {verifyToken} = require("../middlewares/verifyToken")
-const {validateInput} = require("../middlewares/validateInput")
-const {createAccount,getOneAccount,getAccounts,deleteAccount,updateAccount}= require ("../controllers/accounts")
-const {validateAccount} = require ("../models/Accounts").default 
-const router = express.Router() ;
+const express = require("express");
+const { verifyToken } = require("../middlewares/verifyToken");
+const { validateInput } = require("../middlewares/validateInput");
+const {
+  createAccount,
+  getOneAccount,
+  getAccounts,
+  deleteAccount,
+  updateAccount,
+} = require("../controllers/accounts");
+const { validateAccount } = require("../models/Accounts").default;
+const router = express.Router();
 
 // router.all ('/*', verifyToken )
-router.post ('/',verifyToken,validateInput(validateAccount),createAccount);
-router.get ('/',verifyToken,getAccounts);
-router.get ('/:id',verifyToken,getOneAccount);
-router.delete ('/:id',verifyToken,deleteAccount);
-router.patch ('/:id',verifyToken,updateAccount);
+router.post("/", verifyToken, validateInput(validateAccount), createAccount);
+router.get("/", verifyToken, getAccounts);
+router.get("/:id", verifyToken, getOneAccount);
+// router.delete ('/:id',verifyToken,deleteAccount);
+router.post("/remove", verifyToken, deleteAccount);
+router.post("/update", verifyToken, updateAccount);
 
-
-
-module.exports = router; 
-
-
-
-
-
+module.exports = router;
